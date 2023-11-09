5291982d8c69b5e8ac1e9af1b6b7eb04d91e4f14
unknown <yufi7370@gmail.com>
2023-11-09 23:08:47 +0200

    delete line

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,7 @@ function App() {
       <div>
         <Routes>
           <Route
-              path={'/'}
-              
+              path={'/'}   
               element={<Home/>}
           />
         </Routes>
